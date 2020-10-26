f327ad3e13bbc73e4ce2ea6ddd6409fc55320b64
xian0310567 <xian0310567@naver.com>
2020-10-26 18:30:23 +0900

    html,css, js_index 작업 시작

---
+++ javascript/index.js
@@ -0,0 +1,9 @@
+$(function (){
+    $(document).ready(function() {
+        setInterval(function() {
+            $('#Our_Service').animate({
+                //
+            })
+        }, 5000);
+    });
+});
